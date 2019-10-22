2d0f62486e13e3cc45a1509e10bd568edf35fb32
Sean Aung <seanaung.21@gmail.com>
2019-10-21 21:33:33 -0500

    Added ex2 code into JavaScript file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,6 +4,36 @@ function startBoard() {
         drawBoard[i].setAttribute("class", "square");
     }
 }
-window.onload = startBoard;
+let game = new Array();
 
+function displayXorO() {
+    let cells = document.querySelectorAll("#board > div");
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].onclick = function() {
+            if(game.length === 0) {
+                cells[i].classList.add("square", "X");
+                cells[i].innerHTML = "X";
+                game.push("X");
+            }
+            else {
+                if (game[game.length - 1] === "X"){
+                    cells[i].classList.add("square", "O");
+                    cells[i].innerHTML = "O";
+                    game.push("O");
+                }
+                else if (game[game.length - 1] === "O"){
+                    cells[i].classList.add("square", "X");
+                    cells[i].innerHTML = "X";
+                    game.push("X");
+                }
+            }
+        }
+    }
+}
 
+function start() {
+    startBoard();
+    displayXorO();
+}
+
+window.onload = start;
